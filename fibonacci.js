95c9952292cdb2141c216a65594a8c87f15c43f1
James Hershey <jameshershey34@gmail.com>
2024-01-31 16:15:53 -0500

    solved fibonacci.js and used jest to test it

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
-  return 0;
+  let fibNumbers = [0, 1]; // Initialize the first two Fibonacci numbers
+    for (let i = 2; i <= num; i++) {
+        fibNumbers.push(fibNumbers[i - 2] + fibNumbers[i - 1]); // Compute the next Fibonacci number
+    }
+    return fibNumbers[num];
 }
+
 
 module.exports = fibonacci;
